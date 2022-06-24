116524e15f9127f47bc3048bd229d50a19f599b7
Yaqoob <sualehfarooq65@gmail.com>
2022-06-23 22:46:42 -0700

    Commit
    JS Class Tasks
    Front UI Created
    Head & Tails Game Created

---
+++ Head & Tails Game/app.js
@@ -0,0 +1,18 @@
+function game() {
+    document.getElementById("input").value;
+
+    var a = prompt("Enter Player 1 Name")
+    var select = prompt("Please Select Head or Tail ??   H/T").toUpperCase();
+    var b = prompt("Enter Player 2 Name")
+    var arr = ["H", "T"]
+
+    var random = Math.floor(Math.random() * arr.length);
+
+    var final = arr[random];
+    if (select === final) {
+        document.getElementById("input").innerHTML = ("Congratulations " + a + " Won !")
+
+    } else if (select != final) {
+        document.getElementById("input").innerHTML = ("Congratulations " + b + " Won !")
+    }
+}
