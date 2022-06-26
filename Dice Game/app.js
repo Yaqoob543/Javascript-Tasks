c038abd8cee667867db6ff98bd143dcc568ad04a
Yaqoob <sualehfarooq65@gmail.com>
2022-06-26 03:55:00 -0700

    Commit
    Update functionality of game and fix the title name

--- Dice Game/app.js
+++ Dice Game/app.js
@@ -7,11 +7,7 @@
 function game() {
 
     document.getElementById('output').value;
-    var arr = [1, 2, 3, 4, 5, 6]
-    var a = Math.random(arr) * 6
-    var b = Math.round(a)
+    var a = Math.floor(Math.random() * 6) + 1;
+    document.getElementById('output').innerHTML = (a)
 
-    if (b != 0) {
-        document.getElementById('output').innerHTML = (b)
-    }
 }
